f3184a666831aa0b92b7a8d80dc7ac3d143c3e44
Syed Osama Hussain <osamahussain38@gmail.com>
2020-01-28 22:32:37 +0500

    Viginere Cipher Completed.

--- cipher.js
+++ cipher.js
@@ -3,37 +3,51 @@ const select = document.querySelector("#select"),
   PT = document.querySelector("#PT"),
   key = document.querySelector("#key"),
   btn = document.querySelectorAll(".btn");
+let alpha = {};
 
-btn[0].addEventListener("click", (e) => {
-  switch (parseInt(select.value)) {
-    case 0:
-      shift(e.currentTarget.innerHTML);
-      break;
+window.onload = () => {
+  CT.value = "";
+  PT.value = "";
+  key.value = "";
 
-    case 1:
-      viginere(e.currentTarget.innerHTML);
-      break;
-  }
+}
 
+select.addEventListener("change", (e) => {
+  CT.value = "";
+  PT.value = "";
+  key.value = "";
 }, false);
 
+for (let i = 0; i <= 25; i++) {
+  alpha[String.fromCharCode(65 + i)] = i;
+}
 
-btn[1].addEventListener("click", (e) => {
-  switch (parseInt(select.value)) {
-    case 0:
-      shift(e.currentTarget.innerHTML);
-      break;
+for (let i = 0; i < btn.length; i++) {
+  btn[i].addEventListener("click", (e) => {
+    switch (parseInt(select.value)) {
+      case 0:
+        shift(e.currentTarget.innerHTML);
+        break;
 
-    case 1:
-      viginere(e.currentTarget.innerHTML);
-      break;
-  }
+      case 1:
+        viginere(e.currentTarget.innerHTML);
+        break;
+    }
 
-}, false);
+  }, false);
+}
+
+
+function checkKey(val) {
+  if (val.charCodeAt(0) >= 65 && val.charCodeAt(0) <= 90)
+    return true;
+
+  return false;
+}
+
 
 function shift(btnType) {
   let ptval = "";
-  let alpha = {};
   let ctval = "";
   let length;
 
@@ -46,9 +60,6 @@ function shift(btnType) {
     length = ptval.length;
   }
 
-  for (let i = 0; i <= 25; i++) {
-    alpha[String.fromCharCode(65 + i)] = i;
-  }
 
   for (let i = 0; i < length; i++) {
     let code = "";
@@ -58,7 +69,7 @@ function shift(btnType) {
 
       uppercase = ctval[i].toUpperCase();
 
-      if (uppercase.charCodeAt(0) >= 65 && uppercase.charCodeAt(0) <= 90) {
+      if (checkKey(uppercase)) {
 
         code = (alpha[uppercase] + parseInt(key.value)) % 26;
         ptval += Object.keys(alpha).find(k => alpha[k] === code);
@@ -71,9 +82,13 @@ function shift(btnType) {
     } else {
       uppercase = ptval[i].toUpperCase();
 
-      if (uppercase.charCodeAt(0) >= 65 && uppercase.charCodeAt(0) <= 90) {
+      if (checkKey(uppercase)) {
 
         code = (alpha[uppercase] - parseInt(key.value)) % 26;
+
+        while (code < 0) {
+          code += 26;
+        }
         ctval += Object.keys(alpha).find(k => alpha[k] === code);
 
       } else {
@@ -93,6 +108,89 @@ function shift(btnType) {
 
 };
 
+function constructKey(keyval, text) {
+
+  let len = text.length;
+
+  for (let i = 0; ; i++) {
+    if (len == i)
+      i = 0;
+    if (keyval.length === text.length)
+      break;
+    keyval += keyval[i];
+  }
+  return keyval;
+
+}
+
+
+
 function viginere(btnType) {
-  alert("Viginere");
+  let keyval = key.value;
+  let ptval = "";
+  let ctval = "";
+  let length;
+
+  if (btnType === "Encrypt") {
+    ctval = CT.value;
+    length = ctval.length;
+    keyval = constructKey(keyval, ctval);
+  } else {
+
+    ptval = PT.value;
+    length = ptval.length;
+    keyval = constructKey(keyval, ptval);
+  }
+
+  for (let i = 0; i < length; i++) {
+    let code = "";
+    let uppercase = "";
+
+    if (btnType === "Encrypt") {
+
+      uppercase = ctval[i].toUpperCase();
+
+      if (checkKey(uppercase)) {
+
+        if (!checkKey(keyval[i].toUpperCase())) {
+          alert(`Invalid key ${keyval} `);
+          return;
+        }
+
+        code = (alpha[uppercase] + alpha[keyval[i].toUpperCase()]) % 26;
+        ptval += Object.keys(alpha).find(k => alpha[k] === code);
+
+      } else {
+
+        alert(`Invalid string ${ctval}`);
+        return;
+      }
+    } else {
+      uppercase = ptval[i].toUpperCase();
+
+      if (checkKey(uppercase)) {
+
+        if (!checkKey(keyval[i].toUpperCase())) {
+          alert(`Invalid key ${keyval} `);
+          return;
+        }
+
+        code = (alpha[uppercase] - alpha[keyval[i].toUpperCase()] + 26) % 26;
+
+        ctval += Object.keys(alpha).find(k => alpha[k] === code);
+
+      } else {
+
+        alert(`Invalid string ${ptval}`);
+        return;
+      }
+    }
+  }
+
+  if (btnType === "Encrypt") {
+    PT.value = ptval;
+  } else {
+    CT.value = ctval;
+  }
+
 }
